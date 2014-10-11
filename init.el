4608e5c76eb97ce4675a309bea0d5f8d0a901556
Peter Moresi <peter.moresi@gmail.com>
2014-10-10 18:37:56 -0700

    Updating my init.el with YASnippet and undo-tree

--- init.el
+++ init.el
@@ -6,8 +6,8 @@
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 ;; Load Ruby with RVM
-;;(require 'rvm)
-;;(rvm-use ruby-2.1.3");; use rvm's default ruby for the current Emacs session
+;; (require 'rvm)
+;; (rvm-use ruby-2.1.3");; use rvm's default ruby for the current Emacs session
 
 ;; AVOID WEIRD BEHAVIOR IN PATH ENVIRONMENT VARIABLE Copy PATH from ~/.bashrc
 (let ((path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
@@ -34,11 +34,10 @@
 (org-crypt-use-before-save-magic)
 (setq org-tags-exclude-from-inheritance (quote ("crypt")))
 
-(setq org-crypt-key "A706CA78")
 ;; GPG key to use for encryption
 ;; Either the Key ID or set to nil to use symmetric encryption.
+(setq org-crypt-key "A706CA78")
 
-(setq auto-save-default nil)
 ;; Auto-saving does not cooperate with org-crypt.el: so you need
 ;; to turn it off if you plan to use org-crypt.el quite often.
 ;; Otherwise, you'll get an (annoying) message each time you
@@ -47,12 +46,10 @@
 ;; To turn it off only locally, you can insert this:
 ;;
 ;; # -*- buffer-auto-save-file-name: nil; -*-
-
+(setq auto-save-default nil)
 
 ;; ORG Capture
 (define-key global-map "\C-cc" 'org-capture)
-
-
 (setq org-capture-templates
       '(("w" "Work Task" entry (file+headline "~/org/gtd.org" "Work")
 	 "* TODO %?\n  %T\n  %i\n  %a")
@@ -69,17 +66,38 @@
 (add-to-list 'custom-theme-load-path "/Users/jenmoresi/.emacs.d/elpa/leuven-theme-20140929.1435")
 (load-theme 'leuven t)                  ;; for Emacs 24+
 
-;; Make emacs evil
+
+;; override default undo behavior
+(require 'undo-tree)
+
+;; Make emacs evil (vim-like)
+;; Installed from git://gitorious.org/evil/evil.git
 (add-to-list 'load-path "~/.emacs.d/plugins/evil")
 (require 'evil)
 (evil-mode 1)
 
 ;; load the evil-leader
-(require 'evil-leader')
+;; Installed from https://github.com/cofi/evil-leader 
+(add-to-list 'load-path "~/.emacs.d/plugins/evil-leader")
+(require 'evil-leader)
 
 ;; Make org-mode evil too
+;; Installed from git://github.com/edwtjo/evil-org-mode.git
 (add-to-list 'load-path "~/.emacs.d/plugins/evil-org-mode")
 (require 'evil-org)
+
+;; YASnippet
+;; Installed from https://github.com/capitaomorte/yasnippet
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+
+(setq yas-snippet-dirs
+      '("~/.emacs.d/plugins/yasnippet/snippets/"        ;; snippets packaged with YASnippets
+	"~/.emacs.d/snippets")				;; personal snippets
+        )
+
+
+(yas-global-mode 1)
 
 ;; Markdown Setup
 (autoload 'markdown-mode "markdown-mode"
@@ -88,7 +106,6 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.html.erb\\'" . web-mode))
-
 
 
 (custom-set-variables
@@ -101,10 +118,3 @@
  '(org-babel-load-languages (quote ((R . t) (emacs-lisp . t) (js . t) (ruby . t) (python . t) (sqlite . t) (ledger . t) (C . t) (calc . t) (clojure . t) (org . t) (awk . t) (sql . t))))
  '(org-confirm-babel-evaluate nil))
 (put 'upcase-region 'disabled nil)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
